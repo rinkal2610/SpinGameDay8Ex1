1eda6e10ac29c3f6dcde5aec00a187319eef04c7
rinkal2610 <rinkapatel2233@gmai.com>
2023-07-19 12:17:39 +0530

    initial commit

---
+++ ConAppGameEx1Day8/GameClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppGameEx1Day8
+{
+    public class GameClass
+    {
+         
+        public void Initial()
+        {
+           static int elevel = 1;
+           static int probability = 100;
+        }
+        public void Firstspin()
+        {
+            elevel = elevel + 1;
+            probability += probability + 10;
+        }
+        public void Secondspin()
+        {
+            elevel = elevel + 2;
+            probability += probability + 20;
+        }
+        public void Thirdspin()
+        {
+            elevel = elevel - 3;
+            probability += probability - 30;
+        }
+        public void Fourthtspin()
+        {
+            elevel = elevel + 4;
+            probability += probability + 0;
+        }
+       
+        public void Fifthspin()
+        {
+            elevel = elevel - 5;
+            probability += probability -50;
+        }
+        public void Sixthspin()
+        {
+            elevel = elevel - 1;
+            probability += probability - 60;
+        }
+        public void Seventhspin()
+        {
+            elevel += elevel + 1;
+            probability += probability + 70;
+        }
+        public void Eighthspin()
+        {
+            elevel += elevel - 2;
+            probability += probability - 20;
+        }
+        public void Ninthspin()
+        {
+            elevel += elevel - 3;
+            probability += probability - 30;
+        }
+        public void Tenthspin()
+        {
+            elevel += elevel + 10;
+            probability += probability + 100;
+        }
+    }
+}
---
+++ ConAppGameEx1Day8/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppGameEx1Day8
+{
+    public delegate void OurDel();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GameClass game = new GameClass();
+            OurDel del = new OurDel(game.Initial);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter your lucky number");
+                double lucky = double.Parse(Console.ReadLine());
+                switch (lucky)
+                {
+                    case 1:
+                        {
+                            del += new OurDel(game.Firstspin);
+                            break;
+                        }
+                    case 2:
+                        {
+                            del += new OurDel(game.Secondspin);
+                            break;
+                        }
+                    case 3:
+                        {
+                            del += new OurDel(game.Thirdspin);
+                            break;
+                        }
+                    case 4:
+                        {
+                            del += new OurDel(game.Fourthtspin);
+                            break;
+                        }
+                    case 5:
+                        {
+                            del += new OurDel(game.Fifthspin);
+                            break;
+                        }
+                    case 6:
+                        {
+                            del += new OurDel(game.Sixthspin);
+                            break;
+                        }
+                    case 7:
+                        {
+                            del += new OurDel(game.Seventhspin);
+                            break;
+                        }
+                    case 8:
+                        {
+                            del += new OurDel(game.Eighthspin);
+                            break;
+                        }
+                    case 9:
+                        {
+                            del += new OurDel(game.Ninthspin);
+                            break;
+                        }
+                    case 10:
+                        {
+                            del += new OurDel(game.Tenthspin);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("wrong choice!");
+                            break;
+                        }
+                }
+            }
+            if (elevel >= 4 && prob > 60)
+                Console.WriteLine("You Win!");
+            else if(energy>=1 && prob>50)
+                Console.WriteLine("You are runner up!");
+            else
+                Console.WriteLine("You loose!");
+            Console.ReadKey();
+        }
+    }
+}
